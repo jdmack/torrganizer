18adba3857a20af71dc0a902bd4707ae1873fa03
James Mack <jdmack@ucsd.edu>
2013-02-05 05:28:36 -0800

    Create classes for File objects

---
+++ ShowFile.java
@@ -0,0 +1,92 @@
+//=============================================================================
+// TShow.java
+//=============================================================================
+
+//===============================
+// Imports
+//===============================
+
+
+public class TShow extends TFile
+{
+    //===============================================================
+    //      Constants
+    //===============================================================
+
+
+    //===============================================================
+    //      Instance Variables
+    //===============================================================
+    private int    seasonNumber;
+    private int    episodeNumber;
+    private String showTitle
+
+    //===============================================================
+    //      Constructors
+    //===============================================================
+
+    public TShow(String name, String extension, int season, int episode, title)
+    {
+        super(name, extension);
+
+        seasonNumber  = season;
+        episodeNumber = episode;
+        showTitle     = title;
+    }
+
+    //===============================================================
+    //      getters / setters
+    //===============================================================
+
+    //=================================
+    //      seasonNumber
+    //=================================
+
+    public int getSeasonNum()
+    {
+        return seasonNumber;
+    }
+
+    public void setSeasonNum(int season)
+    {
+        seasonNumber = season;
+    }
+
+    //=================================
+    //      episodeNumber
+    //=================================
+
+    public String getEpisodeNum()
+    {
+        return fileName;
+    }
+
+
+    public void setEpisodeNum(int episode)
+    {
+        episodeNumber = episode;
+    }
+    
+    //=================================
+    //      showTitle
+    //=================================
+
+    public String getShowTitle()
+    {
+        return showTitle;
+    }
+
+
+    public void setShowTitle(String title)
+    {
+        showTitle = title;
+    }
+    
+    //===============================================================
+    //      conditionals
+    //===============================================================
+
+    public boolean isShow() { return true; }
+
+
+}
