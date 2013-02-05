f3384d48f321f4849a4238f5cf57cd89b074ce3a
James Mack <jdmack@ucsd.edu>
2013-02-05 05:29:02 -0800

    Fix errors for compilation

--- Torrganizer.java
+++ Torrganizer.java
@@ -6,16 +6,39 @@
 //
 //=============================================================================
 
-//=================
+//===============================
 // Imports
-//=================
+//===============================
 
+// Library
 import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+// Classes
+//import TFile;
+//import MovieFile;
+//import ShowFile;
 
 
 public class Torrganizer
 {
+    //===============================================================
+    //      Constants
+    //===============================================================
+
+    // Directories
     private static final String DEFAULT_PATH = "/home/james";
+
+    // Regex
+    private static final String REGEX_SHOW1 = "([a-zA-Z0-9\\.\\ ]+)(S\\d\\dE\\d\\d).*\\.(mp4|mpeg4|mp3|avi|mkv|wmv)";
+    private static final String REGEX_SHOW2 = "([a-zA-Z0-9\\.\\ ]+)(\\d\\d\\d).*\\.(mp4|mpeg4|avi|mkv)";
+    private static final String REGEX_MOVIE = "([a-zA-Z0-9\\.\\ ]+).*[\\(|\\]]?(19|20\\d\\d)[\\(|\\]]?.*\\.(mp4|mpeg4|avi|mkv)";
+
+
+    //===============================================================
+    //      main
+    //===============================================================
 
     public static void main(String [] args)
     {
@@ -23,8 +46,51 @@ public class Torrganizer
         String [] files = homeDir.list();
         File [] filePaths = homeDir.listFiles();
 
+
+        MovieFile movie = new MovieFile("The", "mp4", 2014);
+
         printFiles(filePaths, 0);
     }
+
+
+    //===============================================================
+    //      matchFile
+    //===============================================================
+
+    public static void matchFile(File thisFile)
+    {
+        //Pattern show1 = Pattern.compile();
+
+        //Matcher matcher = pattern.matcher(thisFile.getName());
+
+    }    
+
+
+    /****************************************************************
+     
+    Video
+    ([a-zA-Z0-9\.\ ]+)(S\d\dE\d\d).*\.(mp4|mpeg4|avi|mkv)
+
+    MP3
+
+
+     
+    
+     
+     
+     
+     
+     
+    ****************************************************************/
+
+
+
+
+
+
+    //===============================================================
+    //      printFiles
+    //===============================================================
 
     public static void printFiles(File [] files, int indentLevel)
     {
